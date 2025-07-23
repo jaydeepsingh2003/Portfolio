7bb0bb51ea07dffb9190c296681a61e72625b8c4
Jaydeep <someone@email.com>
2025-07-23 20:46:13 +0530

    Describe what you changed

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import Tech from './Tech';
@@ -22,6 +22,7 @@ export {
   Contact,
   EarthCanvas, 
   BallCanvas, 
+  ComputersCanvas, 
   StarsCanvas,
   Profile,
   Footer,
